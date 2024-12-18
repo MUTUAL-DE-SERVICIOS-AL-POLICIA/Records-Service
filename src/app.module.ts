551d30bed4c07a09f68b738b5797dabaff7b13ef
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-18 11:23:37 -0400

    archivo de dockerizacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CommonModule],
+  imports: [ConfigModule.forRoot(),DatabaseModule , CommonModule],
   controllers: [],
   providers: [],
 })
