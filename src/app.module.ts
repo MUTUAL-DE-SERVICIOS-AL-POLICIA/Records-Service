5c5d33ede8e0cef5434565dfbdfee388fcbe73a1
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-01 09:17:22 -0400

    INTEGRACION DE MODULOS APPMOBILE,BENEFICIARIES,KIOSK,AUTHMODULE

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
+import { RecordsAppMobileModule } from './records-app-mobile/records-app-mobile.module';
+import { RecordsBeneficiariesModule } from './records-beneficiaries/records-beneficiaries.module';
+import { RecordsKioskModule } from './records-kiosk/records-kiosk.module';
+import { RecordsAuthModule } from './records-auth/records-auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, CommonModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, CommonModule, RecordsAppMobileModule, RecordsBeneficiariesModule, RecordsKioskModule, RecordsAuthModule],
   controllers: [],
   providers: [],
 })
