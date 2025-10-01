7a8e261c587d6faf2b572eab3f221c4d76533f84
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-01 09:20:13 -0400

    CREACIÓN DE LA ENTIDAD DE LOS MODULOS DE LAS TABLAS CORRESPONDIENTES A SUS RECORDS

---
+++ src/records-kiosk/records-kiosk.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class RecordsKiosk {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 150 })
+  action: string;
+
+  @Column({ type: 'text', nullable: true })
+  description?: string;
+
+  @Column({ type: 'jsonb', nullable: true })
+  input: any;
+
+  @Column({ type: 'jsonb', nullable: true })
+  output: any;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
