fdb760c29d9720aceba4726b5e9cb18bd70c5f9a
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-01 09:18:10 -0400

    MIGRACIÓN CREACION TABLA RECORD KIOSK

---
+++ src/database/migrations/1758826099329-createRecordKiosk.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateRecordKiosk1758826099329 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'records_kiosk',
+                columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'action',
+                    type: 'varchar',
+                    length: '150',
+                    isNullable: false,
+                },
+                {
+                    name: 'description',
+                    type: 'text',
+                    isNullable: true,
+                },
+                {
+                    name: 'input',
+                    type: 'jsonb',
+                    isNullable: true,
+                },
+                {
+                    name: 'output',
+                    type: 'jsonb',
+                    isNullable: true,
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'CURRENT_TIMESTAMP',
+                },
+                ],
+            }),
+            true,
+            );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('records_kiosk');
+    }
+
+}
