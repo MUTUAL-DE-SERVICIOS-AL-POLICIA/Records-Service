04ee20fc8aaf42a7b7865d7a114c2d264b479807
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-01 09:17:57 -0400

    MIGRACIÓN CREACION TABLA RECORD BENEFICIARIES

--- src/database/migrations/1757962412640-createRecordBeneficiaries.ts
+++ src/database/migrations/1757962412640-createRecordBeneficiaries.ts
@@ -1,48 +1,67 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateRecordBeneficiaries1757962412640 implements MigrationInterface {
+export class CreateRecordBeneficiaries1757962412640
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'records_beneficiaries',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'user',
+            type: 'jsonb',
+            isNullable: true,
+          },
+          {
+            name: 'action',
+            type: 'varchar',
+            length: '150',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'text',
+            isNullable: true,
+          },
+          {
+            name: 'input',
+            type: 'jsonb',
+            isNullable: true,
+          },
+          {
+            name: 'output',
+            type: 'jsonb',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+      true,
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: 'records_beneficiaries',
-                columns: [
-                {
-                    name: 'id',
-                    type: 'int',
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: 'increment',
-                },
-                {
-                    name: 'action',
-                    type: 'varchar',
-                    length: '150',
-                    isNullable: false,
-                },
-                {
-                    name: 'description',
-                    type: 'text',
-                    isNullable: true,
-                },
-                {
-                    name: 'metadata',
-                    type: 'jsonb',
-                    isNullable: true,
-                },
-                {
-                    name: 'created_at',
-                    type: 'timestamp',
-                    default: 'CURRENT_TIMESTAMP',
-                },
-                ],
-            }),
-            true,
-        );
-    }
+    await queryRunner.query(`
+      CREATE INDEX idx_records_beneficiaries_affiliate_id
+      ON records.records_beneficiaries (((input->'user'->>'affiliateId')::bigint));
+    `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('records_beneficiaries');
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX IF EXISTS idx_records_beneficiaries_affiliate_id`,
+    );
 
+    await queryRunner.dropTable('records_beneficiaries');
+  }
 }
