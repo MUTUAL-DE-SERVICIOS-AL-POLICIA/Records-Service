955b60ed3c5041098e20aed1aa02698ff6e3efc3
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-02 17:12:22 -0400

    CREACIÓN DE DICCIONARIO

--- src/records-app-mobile/records-app-mobile.dictionary.ts
+++ src/records-app-mobile/records-app-mobile.dictionary.ts
@@ -1,13 +1,19 @@
 export const actionMessages: Record<string, string> = {
-  "GET: AppMobileController.informationLoan":
-    "{fullname} obtuvo la información de sus préstamos exitosamente.",
+  'POST: AppMobileController.version':
+    'Ruta pública, se verificó la versión de la aplicación móvil.',
+  'POST: AppMobileController.ecoComLivenessStore':
+    'Inició el proceso de enrolamiento y verificación para la creación de complementos económicos para el afiliado con NUP {affiliateId}',
+  'POST: AppMobileController.ecoComEconomicComplementsStore':
+    'Creo complementos económicos para el afiliado con NUP {affiliateId}',
+  'GET: AppMobileController.ecoComSaveIdentity':
+    'Verifico la identidad del afiliado de complemento con NUP {affiliateId}',
 };
 
 export function translateAction(
   action: string,
   user: Record<string, any> = {},
   input: Record<string, any> = {},
-  output: Record<string, any> = {}
+  output: Record<string, any> = {},
 ): string {
   const template = actionMessages[action];
   if (!template) {
@@ -15,9 +21,7 @@ export function translateAction(
   }
 
   return template.replace(/\{(\w+)\}/g, (_, key) => {
-    const value =
-      user?.[key] ??
-      "N/A";
+    const value = user?.[key] ?? output?.[key] ?? input?.params?.[key] ?? 'N/A';
     return String(value);
   });
 }
