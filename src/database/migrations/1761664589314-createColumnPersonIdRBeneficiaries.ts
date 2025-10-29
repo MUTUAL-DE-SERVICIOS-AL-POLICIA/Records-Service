ee061cbd0c6cf22a6dc3fb28542945348d215c21
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-29 16:55:09 -0400

    creacion de migración para crear columna personID y llenarlos

---
+++ src/database/migrations/1761664589314-createColumnPersonIdRBeneficiaries.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateColumnPersonIdRBeneficiaries1761664589314 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE records.records_beneficiaries
+            ADD COLUMN person_id BIGINT NULL;
+        `);
+
+        await queryRunner.query(`
+            UPDATE records.records_beneficiaries rb
+            SET person_id = pa.person_id
+            FROM beneficiaries.person_affiliates pa
+            WHERE pa.type = 'affiliates'
+                AND (rb.input->'params'->>'affiliateId')::bigint = pa.type_id;
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE records.records_beneficiaries
+            DROP COLUMN person_id;
+        `);
+    }
+}
